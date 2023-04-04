2d9769ea9391d893642ab1215a40403bbfd0f718
hamsanguk <gtd0906@gmail.com>
2023-04-04 23:03:16 +0900

    hansomevue1

---
+++ src/components/Header.vue
@@ -0,0 +1,59 @@
+<template>
+    <header>
+        <div class="container">
+            <h1>
+                <img src="../../public/images/logo.png" alt="한솜바이크">
+            </h1>
+            <ul class="gnb">
+                <li>한글이</li>
+                <li>아니라서</li>
+                <li>그른가?</li>
+            </ul>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../scss/main.scss";
+@font-face {
+  font-family: 'Noto Sans KR', sans-serif;
+  font-weight: 300;
+  src: url('../assets/font/NSans/NotoSansKR-Light.woff') format('woff');
+} 
+    $color-r:#900;
+    header{
+        background-color: #888;
+        .container{
+        width: 1200px;
+        border:1px solid $color-r;
+        h1{
+            text-align: center;
+            outline: 1px solid blue;
+            margin: 0;
+        }
+        .gnb{
+            display: flex;
+            width: 50%;
+            justify-content: space-between;
+            margin: 0 auto;
+            li{
+                font-family: 'Noto Serif KR','sans-serif';
+                font-weight: 500;
+                line-height: 40px;
+                border:1px solid orange;
+                &:nth-child(2){
+                    font-family: 'Noto Sans KR';
+                    font-weight: 500;
+                    color:#900;
+                }
+            }
+        }
+    }
+    }
+</style>
